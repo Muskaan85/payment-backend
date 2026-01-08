730119eb3bcc832e3321f88425021b56ec03874d
Muskaan <99648776+Muskaan85@users.noreply.github.com>
2026-01-08 09:46:23 +0000

    Updated backend code

--- app/database.py
+++ app/database.py
@@ -1,31 +1,8 @@
-# from sqlalchemy import create_engine
-# from sqlalchemy.orm import sessionmaker, declarative_base
-# import os
-# from dotenv import load_dotenv
-# load_dotenv()
-
-
-# DATABASE_URL = os.getenv("DATABASE_URL")
-# if not DATABASE_URL:
-#     raise ValueError("DATABASE_URL environment variable is not set")
-# engine = create_engine(DATABASE_URL)
-# SessionLocal = sessionmaker(bind=engine)
-
-# Base = declarative_base()
-
-# def get_db():
-#     db = SessionLocal()
-#     try:
-#         yield db
-#     finally:
-#         db.close()
-
-
 import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = os.getenv("DATABASE_URL")  # Read from env
+DATABASE_URL = os.getenv("DATABASE_URL")  
 
 if not DATABASE_URL:
     # Fallback for local dev
--- app/main.py
+++ app/main.py
@@ -1,29 +1,3 @@
-# from fastapi import FastAPI
-# from app.routes.payment import router
-# from app.database import engine
-# from app.models import Base
-
-# Base.metadata.create_all(bind=engine)
-
-# app = FastAPI(title="Cashfree Payment API")
-# app.add_middleware(
-#     CORSMiddleware,
-#     allow_origins=[
-#         "http://localhost:3000",
-#         "https://ubiquitous-system-6x5vg6977j53rrgw-3000.app.github.dev",
-#         "https://*.vercel.app"
-#     ],
-#     allow_credentials=True,
-#     allow_methods=["*"],
-#     allow_headers=["*"],
-# )
-
-# app.include_router(router)
-
-# @app.get("/")
-# def root():
-#     return {"status": "Backend running"}
-
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
@@ -40,7 +14,8 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:3000",
         "https://ubiquitous-system-6x5vg6977j53rrgw-3000.app.github.dev",
-        "https://your-vercel-app.vercel.app",  # 👈 ADD EXACT DOMAIN
+        "https://your-vercel-app.vercel.app",  
+        "https://mqmclz-3000.csb.app"
     ],
     allow_credentials=True,
     allow_methods=["*"],
